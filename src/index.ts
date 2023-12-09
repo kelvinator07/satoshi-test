e69a9f7c75c6bb170943cef7bacf2af53711dc84
Kelvin Isievwore <kelvinator4leo@gmail.com>
2023-12-09 02:57:40 +0100

    Server Init

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import express, { Application } from "express";
+import cors, { CorsOptions } from "cors";
+import Routes from "./routes";
+import Database from "./db";
+import CoinMarketService from "./service/coinmarket.service";
+
+export default class Server {
+  constructor(app: Application) {
+    this.config(app);
+    this.syncDatabase();
+    new Routes(app);
+    this.startService();
+  }
+
+  private config(app: Application): void {
+    const corsOptions: CorsOptions = {
+      origin: "http://localhost:8081",
+    };
+
+    app.use(cors(corsOptions));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+  }
+
+  private syncDatabase(): void {
+    const db = new Database();
+    db.sequelize?.sync();
+  }
+
+  private startService() {
+    new CoinMarketService();
+  }
+}
